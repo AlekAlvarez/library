90391f67b13bee4e01e188d92c68b934adb0dc04
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-06-03 10:50:43 -0500

    Creates library array and book class

---
+++ script.js
@@ -0,0 +1,21 @@
+const myLibrary=[];
+function Book(author,title,pageNumbers,isRead,index){
+    this.author=author;
+    this.title=title;
+    this.pageNumbers=pageNumbers;
+    this.isRead=isRead;
+    this.index=index;
+    this.decrementIndex=function(){
+        this.pageNumbers--;
+    };
+}
+function addBookToLibrary(author,title,pageNumbers,isRead){
+    let book=new Book(author,title,pageNumbers,isRead,myLibrary.length);
+    myLibrary.push(book);
+}
+addBookToLibrary("Kafka",'Metamorphisis',40,true);
+addBookToLibrary('Tolkien','Hobbit',300,true);
+addBookToLibrary("Asimov","The Foundations Edge",250,true);
+for(let i=0;i<myLibrary.length;i++){
+    console.log(myLibrary[i]);
+}
