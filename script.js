b85520f0b9c31fc69cb75405922ac45566e77cb7
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-06-05 14:33:17 -0500

    Completes javascript for adding books to table

--- script.js
+++ script.js
@@ -3,6 +3,30 @@ const addButton=document.querySelector("#add-book");
 const body=document.querySelector("body");
 const container=document.querySelector("#container");
 const form=document.querySelector("#form");
+const submit=document.querySelector("#Submit");
+const author=document.querySelector("#author");
+const title=document.querySelector("#title");
+const pageNumbers=document.querySelector("#pageNumbers");
+const isRead=document.querySelector("#isRead");
+const table=document.querySelector("tbody");
+isRead.addEventListener("click",()=>{
+    if(isRead.checked){
+    isRead.value='read';
+    }
+    else{
+        isRead.value='unread';
+    }
+    console.log(isRead.value);
+})
+submit.addEventListener("click",()=>{
+    container.removeChild(form);
+    addBookToLibrary(author.value,title.value,pageNumbers.value,isRead.value);
+    author.value="";
+    title.value="";
+    pageNumbers.value="";
+    isRead.value="";
+    isRead.checked=false;
+})
 container.removeChild(form);
 addButton.addEventListener("click",()=>{
     container.appendChild(form);
@@ -19,11 +43,26 @@ function Book(author,title,pageNumbers,isRead,index){
 }
 function addBookToLibrary(author,title,pageNumbers,isRead){
     let book=new Book(author,title,pageNumbers,isRead,myLibrary.length);
+    let row=document.createElement("tr");
+    let b=document.createElement("td");
+    b.innerText=myLibrary.length+1;
+    row.appendChild(b);
+    let a=document.createElement("td");
+    a.innerText=author;
+    row.appendChild(a);
+    let t=document.createElement("td");
+    t.innerText=title;
+    row.appendChild(t);
+    let n=document.createElement("td");
+    n.innerText=pageNumbers;
+    row.appendChild(n);
+    let r=document.createElement("td");
+    r.innerText=isRead;
+    row.appendChild(r);
+    let tog=document.createElement("td");
+    row.appendChild(tog);
+    let rem=document.createElement("td");
+    row.appendChild(rem);
+    table.appendChild(row);
     myLibrary.push(book);
-}
-addBookToLibrary("Kafka",'Metamorphisis',40,true);
-addBookToLibrary('Tolkien','Hobbit',300,true);
-addBookToLibrary("Asimov","The Foundations Edge",250,true);
-for(let i=0;i<myLibrary.length;i++){
-    console.log(myLibrary[i]);
 }
